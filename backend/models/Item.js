cbc94c7030226e78a0a3ad2086c0831e325daac0
Martin Daniel Benitez <b.martindaniel@gmail.com>
2023-01-30 23:45:11 +0000

    Fixed placeholder in backend

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -51,7 +51,7 @@ ItemSchema.methods.toJSONFor = function(user) {
     slug: this.slug,
     title: this.title,
     description: this.description,
-    image: this.image || fallbackImage,
+    image: this.image || '/placeholder.png',
     createdAt: this.createdAt,
     updatedAt: this.updatedAt,
     tagList: this.tagList,
