fe93286e4fa21f04116823650c1d60192cc8585f
WilcoApp <hello@wilco.gg>
2023-02-02 19:30:44 +0000

    Robocrop

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,17 +1,7 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
-import agent from "../../agent";
 
-const Banner = (props) => {
-  const onSearchChange = (event) => {
-    props.onSearchFilter(
-      event.target.value, 
-      (page) => 
-      agent.Items.byTitle(event.target.value),
-      agent.Items.byTitle(event.target.value)
-      
-    );
-  };
+const Banner = () => {
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
@@ -19,9 +9,6 @@ const Banner = (props) => {
         <div>
           <span>A place to </span>
           <span id="get-part">get</span>
-          <form>
-            <input  type="text" id="search-box" placeholder="What is it that you truly desire?" name="term" onChange={onSearchChange}/>
-          </form>
           <span> the cool stuff.</span>
         </div>
       </div>
