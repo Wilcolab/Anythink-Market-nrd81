2686129395fa1ddc15643e5522d11ff370344bcc
Martin Daniel Benitez <b.martindaniel@gmail.com>
2023-02-11 01:25:08 +0000

    update

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -8,16 +8,23 @@ output_file="output.txt"
 
 # Check if the file exists
 if [ -f $file ]; then
+  echo "$file exists"
   # Read each line of the file
   while IFS=',' read -r name last email; do
     # Check if the email is from @amazon.com
     if [[ $email == *"@amazon.com" ]]; then
       # Print the name and last name to the output file
-      echo "Name: $name" >> $output_file
-      echo "Last Name: $last" >> $output_file
+      echo "Name: $name" >> $output_file 2> error.txt
+      echo "Last Name: $last" >> $output_file 2> error.txt
     fi
   done < $file
-  echo "The filtered names and last names have been written to $output_file."
+  if [ -s error.txt ]; then
+    echo "There was an error while writing to the file."
+    echo "The error message is:"
+    cat error.txt
+  else
+    echo "The filtered names and last names have been written to $output_file."
+  fi
 else
   # If the file does not exist, print an error message
   echo "Error: The file $file does not exist."
